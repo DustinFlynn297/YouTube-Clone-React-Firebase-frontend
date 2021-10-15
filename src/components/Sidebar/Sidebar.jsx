0c42eb48e9b4273c453f12bd234167fb0301096d
Dustin Flynn <flynnd297@gmail.com>
2021-10-15 17:53:48 -0500

    Created several components for the header and added styling.

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Sidebar.css'
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h1>This is a sidebar</h1>
+        </div>
+    )
+}
+export default Sidebar;
